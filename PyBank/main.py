b89687a6603b3047926019c165a63a891409a776
paulbrichta <paulbrichta91@gmail.com>
2022-10-17 14:04:59 -0500

    adding my work before class hopefully will have time to work on it later

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,28 +3,43 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath) as csvfile:
+with open(csvpath, 'r', newline='') as csvfile:
 
 	csvreader = csv.reader(csvfile, delimiter=',')
 
 	csv_header = next(csvreader)
 
-	profit = next(csvreader)
-
 	months = 0
+	netprofit = 0
+	totalchange = 0
+	averageChange = 0
+	changecounter = 0
+	greatestIncrease = 0
+	greatestDecrease = 0
 	
+	previousprofit = 0
+
 	for line in csvreader:
 		months += 1
-		profit += line[1]
+		netprofit += int(line[1])
+
+		currentprofit = int(line[1])
+
+		if previousprofit != 0:
+			change = currentprofit - previousprofit
+			totalchange += change
+			changecounter += 1
+			greatestIncrease = currentprofit
+		previousprofit = currentprofit
 	
 	print('Financial Analysis')
 	print('----------------------------')
 	print(f'Total Months: {months}')
 
-	print(f'Total: ${profit}')
+	print(f'Total: ${totalchange}')
 
-	print(f'Average Change: ')
+	print(f'Average Change: ${totalchange}')
 
-	print(f'Greatest Increase in Profits: ')
+	print(f'Greatest Increase in Profits: ${greatestIncrease}')
 
-	print(f'Greatest Decrease in Profits:')
+	print(f'Greatest Decrease in Profits: ${greatestDecrease}')
